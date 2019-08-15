f538244ac47062f5c3804659f6f0c8356fa3307b
wzjgo1 <wzjgo1023@163.com>
2019-08-15 23:14:05 +0800

    dep -> mod (#67)
    * dep -> mod
    * and add repos model

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/wzjgo1/go-bitbucket
+
+go 1.12
+
+require (
+	github.com/golang/protobuf v1.0.0
+	github.com/k0kubun/pp v2.3.0+incompatible
+	github.com/mattn/go-colorable v0.0.9
+	github.com/mattn/go-isatty v0.0.3
+	github.com/mitchellh/mapstructure v0.0.0-20180220230111-00c29f56e238
+	golang.org/x/net v0.0.0-20180218175443-cbe0f9307d01
+	golang.org/x/oauth2 v0.0.0-20180227000427-d7d64896b5ff
+	golang.org/x/sys v0.0.0-20180224232135-f6cff0780e54
+	google.golang.org/appengine v1.0.0
+)
--- repositories.go
+++ repositories.go
@@ -1,5 +1,11 @@
 package bitbucket
 
+import (
+	"encoding/json"
+
+	"github.com/mitchellh/mapstructure"
+)
+
 //"github.com/k0kubun/pp"
 
 type Repositories struct {
@@ -10,27 +16,86 @@ type Repositories struct {
 	Diff               *Diff
 	BranchRestrictions *BranchRestrictions
 	Webhooks           *Webhooks
-	Downloads		   *Downloads
+	Downloads          *Downloads
 	repositories
 }
 
-func (r *Repositories) ListForAccount(ro *RepositoriesOptions) (interface{}, error) {
-	urlStr := r.c.requestUrl("/repositories/%s", ro.Owner)
-	if ro.Role != "" {
-		urlStr += "?role=" + ro.Role
-	}
-	return r.c.execute("GET", urlStr, "")
+type RepositoriesRes struct {
+	Page    int32
+	Pagelen int32
+	Size    int32
+	Items   []Repository
 }
 
-func (r *Repositories) ListForTeam(ro *RepositoriesOptions) (interface{}, error) {
+func (r *Repositories) ListForAccount(ro *RepositoriesOptions) (*RepositoriesRes, error) {
 	urlStr := r.c.requestUrl("/repositories/%s", ro.Owner)
 	if ro.Role != "" {
 		urlStr += "?role=" + ro.Role
 	}
-	return r.c.execute("GET", urlStr, "")
+	repos, err := r.c.execute("GET", urlStr, "")
+	if err != nil {
+		return nil, err
+	}
+	return decodeRepositorys(repos)
+}
+
+func (r *Repositories) ListForTeam(ro *RepositoriesOptions) (*RepositoriesRes, error) {
+	urlStr := r.c.requestUrl("/repositories/%s", ro.Owner)
+	if ro.Role != "" {
+		urlStr += "?role=" + ro.Role
+	}
+	repos, err := r.c.executeRaw("GET", urlStr, "")
+	if err != nil {
+		return nil, err
+	}
+	return decodeRepositorys(repos)
 }
 
 func (r *Repositories) ListPublic() (interface{}, error) {
 	urlStr := r.c.requestUrl("/repositories/", "")
-	return r.c.execute("GET", urlStr, "")
+	repos, err := r.c.execute("GET", urlStr, "")
+	if err != nil {
+		return nil, err
+	}
+	return decodeRepositorys(repos)
+}
+
+func decodeRepositorys(reposResponse interface{}) (*RepositoriesRes, error) {
+	var reposResponseMap map[string]interface{}
+	err := json.Unmarshal(reposResponse.([]byte), &reposResponseMap)
+	if err != nil {
+		return nil, err
+	}
+
+	repoArray := reposResponseMap["values"].([]interface{})
+	var repos []Repository
+	for _, repoEntry := range repoArray {
+		var repo Repository
+		err = mapstructure.Decode(repoEntry, &repo)
+		if err == nil {
+			repos = append(repos, repo)
+		}
+	}
+
+	page, ok := reposResponseMap["page"].(float64)
+	if !ok {
+		page = 0
+	}
+
+	pagelen, ok := reposResponseMap["pagelen"].(float64)
+	if !ok {
+		pagelen = 0
+	}
+	size, ok := reposResponseMap["size"].(float64)
+	if !ok {
+		size = 0
+	}
+
+	repositories := RepositoriesRes{
+		Page:    int32(page),
+		Pagelen: int32(pagelen),
+		Size:    int32(size),
+		Items:   repos,
+	}
+	return &repositories, nil
 }
