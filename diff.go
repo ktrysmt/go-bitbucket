1ce7e7789c4ff9fadc303466c5d0bb861efef70f
Kotaro Yoshimatsu <kotaro.yoshimatsu@gmail.com>
2020-01-21 16:40:15 +0900

    fix GetDiff to use executeRaw() internally. (#84)

--- diff.go
+++ diff.go
@@ -6,7 +6,7 @@ type Diff struct {
 
 func (d *Diff) GetDiff(do *DiffOptions) (interface{}, error) {
 	urlStr := d.c.requestUrl("/repositories/%s/%s/diff/%s", do.Owner, do.RepoSlug, do.Spec)
-	return d.c.execute("GET", urlStr, "")
+	return d.c.executeRaw("GET", urlStr, "diff")
 }
 
 func (d *Diff) GetPatch(do *DiffOptions) (interface{}, error) {
