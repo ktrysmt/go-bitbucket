9a42ecc0a24eda988ed3cb6e0f56cb52cf5a7166
Kent R. Spillner <kspillner@acm.org>
2020-12-31 13:32:37 -0600

    add support for deleting pipeline variables
    BitBucket's v2.0 API supports deleting pipeline variables by uuid.
    On success, an empty response body with HTTP status 204 is returned.
    
    Signed-off-by: Kent R. Spillner <kspillner@acm.org>

--- tests/repository_test.go
+++ tests/repository_test.go
@@ -81,3 +81,51 @@ func TestGetRepositoryPipelineVariables(t *testing.T) {
 		t.Error("Cannot list pipeline variables")
 	}
 }
+
+func TestDeleteRepositoryPipelineVariables(t *testing.T) {
+
+	user := os.Getenv("BITBUCKET_TEST_USERNAME")
+	pass := os.Getenv("BITBUCKET_TEST_PASSWORD")
+	owner := os.Getenv("BITBUCKET_TEST_OWNER")
+	repo := os.Getenv("BITBUCKET_TEST_REPOSLUG")
+
+	if user == "" {
+		t.Error("BITBUCKET_TEST_USERNAME is empty.")
+	}
+	if pass == "" {
+		t.Error("BITBUCKET_TEST_PASSWORD is empty.")
+	}
+	if owner == "" {
+		t.Error("BITBUCKET_TEST_OWNER is empty.")
+	}
+	if repo == "" {
+		t.Error("BITBUCKET_TEST_REPOSLUG is empty.")
+	}
+
+	c := bitbucket.NewBasicAuth(user, pass)
+
+	variable := &bitbucket.RepositoryPipelineVariableOptions{
+		Owner:    owner,
+		RepoSlug: repo,
+		Key:      "test_key_to_delete",
+		Value:    "Some value to delete",
+		Secured:  false,
+	}
+
+	res, err := c.Repositories.Repository.AddPipelineVariable(variable)
+	if err != nil {
+		t.Error(err)
+	}
+
+	opt := &bitbucket.RepositoryPipelineVariableDeleteOptions{
+		Owner:    owner,
+		RepoSlug: repo,
+		Uuid:     res.Uuid,
+	}
+
+	// On success the delete API doesn't return any content (HTTP status 204)
+	_, err = c.Repositories.Repository.DeletePipelineVariable(opt)
+	if err != nil {
+		t.Error(err)
+	}
+}
